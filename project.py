0708f4f1667741d19172d276547402f4acdce524
YukikoNii <NIIY64308@gmail.com>
2025-07-06 12:09:57 +0900

    feat: record high_score / refac: made comments easier to read

--- project.py
+++ project.py
@@ -9,44 +9,46 @@ pygame.init()
 
 #screen with width of 400 and height of 700
 screen = pygame.display.set_mode((400,700))
+
 #Set font
 def font_size(int):
-    return pygame.font.Font("myfont.ttf", int) #DESIGN CREDIT:wepfont
+    return pygame.font.Font("myfont.ttf", int) # DESIGN CREDIT:wepfont
 
 font = font_size(30)
+font_small = font_size(15)
 
 
-#Title and icon - CREDIT to Freecodecamp
+# Title and icon - CREDIT to Freecodecamp
 pygame.display.set_caption("Jumping Dog")
 icon = pygame.image.load("dog.tiff")
 pygame.display.set_icon(icon)
 
 
-#Background Music CREDIT: ArturAravidiMusic from Pixabay
+# Background Music CREDIT: ArturAravidiMusic from Pixabay
 pygame.mixer.music.load("background_music.mp3")
-pygame.mixer.music.play(-1)
+pygame.mixer.music.play(-1) # -1 means infinite loop 
 pygame.mixer.music.set_volume(0.5)
 
 
 #VARIABLES-------------------------------------------------
 WIDTH, HEIGHT = screen.get_size()
-score = 0
-num_of_poss = 12
 BLACK = (0,0,0)
+POSITION_COUNT = 12
+high_score = 0
 
 
 #SPRITES---------------------------------------------------
 
 #Blocks sprite
 
-class Block(pygame.sprite.Sprite): #CREDIT to Clear Code (YouTube Channel) for basic structure of sprite.
+class Block(pygame.sprite.Sprite): # CREDIT to Clear Code (YouTube Channel) for basic structure of sprite.
      def __init__(self,x,y):
         super().__init__()
-        self.image = load_image("grass.png") #IMAGE CREDIT: Kenney.nl
+        self.image = load_image("grass.png") # IMAGE CREDIT: Kenney.nl
 
 
         self.image = pygame.transform.scale(self.image,(80,40))
-        #Draws a rectangle around the image.
+        #Draw a rectangle around the image.
         self.rect = self.image.get_rect()
         self.rect.topleft = [x,y]
 
@@ -59,10 +61,10 @@ class Block(pygame.sprite.Sprite): #CREDIT to Clear Code (YouTube Channel) for b
 class Player(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
-        self.image = load_image("dog.tiff") #IMAGE CREDIT: http://mtsids.com/
+        self.image = load_image("dog.tiff") # IMAGE CREDIT: http://mtsids.com/
 
         self.image = pygame.transform.scale(self.image,(50,50))
-         #Draws a rectangle around the image.
+         # Draw a rectangle around the image.
         self.rect = self.image.get_rect()
 
         self.rect.bottomleft = [x,y]
@@ -72,7 +74,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.y -= y
 
     def imgchange(self, image_path):
-        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.image.load(image_path).convert_alpha() # convert_alpha preserves transparency, improves performance  
         self.image = pygame.transform.scale(self.image,(50,50))
 
 
@@ -80,12 +82,12 @@ class Player(pygame.sprite.Sprite):
 class Rock(pygame.sprite.Sprite):
         def __init__(self,x,y):
             super().__init__()
-            self.image = load_image("Rock Pile.png")#IMAGE CREDIT: FunwithPixels on Opengamearts.org
+            self.image = load_image("Rock Pile.png")  # IMAGE CREDIT: FunwithPixels on Opengamearts.org
 
             self.image = pygame.transform.scale(self.image,(50,50))
-            #Draws a rectangle around the image.
+        
+            #Draw a rectangle around the image.
             self.rect = self.image.get_rect()
-
             self.rect.bottomleft = [x,y]
 
 
@@ -104,36 +106,41 @@ class Rock(pygame.sprite.Sprite):
 
 #show gameover text
 def gameover_text():
-    #CREDIT to freecodecamp  (same for show_score())
+    # CREDIT to freecodecamp  (same for show_score())
     text = font.render("Game Over", True, BLACK)
     screen.blit(text,(120,350))
 
 
 #Format score
-def format_score(score):
-    return "Score: "+ str(score)
+def format_score(label, score):
+    return label + ": " + str(score)
 
 #show score
-def show_score(color=BLACK, x=10,y=20):
-    score_num = font.render(format_score(score), True, color)
-    screen.blit(score_num,(x,y))
+def show_score(score, color=BLACK, x=10,y=20):
+    score_text = font.render(format_score("Score", score), True, color)
+    high_score_text = font.render(format_score("High Score", high_score), True, color)
+    screen.blit(score_text,(x,y))
+    screen.blit(high_score_text,(x,60))
+
+
+def set_high_score(high_score, score):
+    return max(high_score, score)
 
 
 #show button
-def show_button():
-    #Image credit: smashingstocks
-    button_image = pygame.image.load("play_button.png") #ICON CREDIT: smashingstocks on Flaticon
+def show_sound_control():
+    # Image credit: smashingstocks
+    button_image = pygame.image.load("play_button.png") # ICON CREDIT: smashingstocks on Flaticon
     button_image = pygame.transform.scale(button_image,(50,50))
-    smaller_font = font_size(15)
-    button_text = smaller_font.render("sound", True, BLACK)
+    text = font_small.render("sound", True, BLACK)
     screen.blit(button_image,(340,20))
-    screen.blit(button_text,(340,10))
+    screen.blit(text,(340,10))
 
 #Produce possible x_positions of blocks
-def produce_positions(num_of_poss):
+def produce_positions(POSITION_COUNT):
     list = []
-    for i in range(num_of_poss):
-        list.append(round(i*330/(num_of_poss-1),1))
+    for i in range(POSITION_COUNT):
+        list.append(round(i*330/(POSITION_COUNT-1),1))
     return list 
 
 def load_image(filename):
@@ -157,7 +164,8 @@ def main():
     jump_y = 0
     fall_y = 0
     background = pygame.image.load("background.png").convert() #IMAGE CREDIT: Zenyu Ren
-    global score
+    score = 0
+    global high_score
 
     #SPRITES -----------------------------------------------------------
     #Create instance of player
@@ -171,7 +179,7 @@ def main():
     block_group = pygame.sprite.Group()
     for i in range(7):
         #Randomly choosing the x-position
-        choices = random.sample(produce_positions(num_of_poss),2)
+        choices = random.sample(produce_positions(POSITION_COUNT),2)
         new_block1 = Block(choices[0],100*i)
         new_block2= Block(choices[1],100*i)
         block_group.add(new_block1)
@@ -203,6 +211,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    print("hello")
+                    running = False
+                    main()
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if 340 < mouse[0] < 390 and 20 < mouse[1] < 70:
                     if playing == True:
@@ -232,36 +246,33 @@ def main():
                 player.rect.x = 350
 
 
-
-
-
-        #Create block rectangles for collision detection
+        # Create block rectangles for collision detection
         rect_list = [block.rect for block in block_group.sprites()]
 
-        #Create rock rectangles for collision detection
+        # Create rock rectangles for collision detection
         rock_rect_list = [rock.rect for rock in rock_group.sprites()]
-        #collsion with the rock
+        # collsion with the rock
 
         if player.rect.inflate(-18,0).collideobjects(rock_rect_list):
             gameover = True
 
 
-        #Only allow the player to jump if it's not already jumping.
+        # Only allow the player to jump if it's not already jumping.
         if not jumping:
-            player.imgchange("dog.tiff") #IMAGE CREDIT: http://mtsids.com/
+            player.imgchange("dog.tiff") # IMAGE CREDIT: http://mtsids.com/
             if keys[pygame.K_SPACE]:
                 jumping = True
                 #Sound Effect from Pixabay
                 if effect_on == True:
-                    sound_effect = pygame.mixer.Sound("sound_effect.mp3") #AUDIO CREDIT: Pixabay
+                    sound_effect = pygame.mixer.Sound("sound_effect.mp3") # AUDIO CREDIT: Pixabay
                     pygame.mixer.find_channel().play(sound_effect)
                     sound_effect.set_volume(0.5)
 
 
         else:
-            player.imgchange("dog2.tiff") #IMAGE CREDIT: http://mtsids.com/
+            player.imgchange("dog2.tiff") # IMAGE CREDIT: http://mtsids.com/
             if 0 <= jump_y <= 100:
-                #If it's jumping, move all the blocks according to a function
+                 #If it's jumping, move all the blocks according to a function
                 for block in block_group:
                     block.move(0,-(-1/7*jump_y+50/7))
                 jump_y += 1
@@ -269,7 +280,7 @@ def main():
                 jump_y = 0
                 jumping = False
 
-        #y-direction
+        # y-direction
         if jumping:
             jump_y += 1
             next = player.rect.collideobjects(rect_list)
@@ -282,9 +293,9 @@ def main():
             jump_y -= 1
 
 
-        #collsion detection and coordinate calculation
+        # collsion detection and coordinate calculation
         if not jumping:
-            #Code: Credits to a user "import random"(on stackoverslow) on the use
+            # Code: Credits to a user "import random"(on stackoverslow) on the use
             if not player.rect.inflate(-18,0).collideobjects(rect_list):
                 for block in block_group:
                     block.move(0,1/40*fall_y)
@@ -294,22 +305,22 @@ def main():
 
 
 
-        #If a block disappears, delete it and create another one, with random x position.
+        # If a block disappears, delete it and create another one, with random x position.
             for block in block_group:
                 if block.rect.y > HEIGHT:
                     if not jumping:
                         block_group.remove(block)
-                        new_target1 = Block(random.choice(produce_positions(num_of_poss)),100)
+                        new_target1 = Block(random.choice(produce_positions(POSITION_COUNT)),100)
                         block_group.add(new_target1)
                         score += 1
 
 
 
-        #if the dog goes out of screen, "Game Over" = True
+        # if the dog goes out of screen, "Game Over" = True
         if player.rect.y > HEIGHT:
-            gameover = True #CREDIT to "Rabbit76" on stackoverflow.
+            gameover = True # CREDIT to "Rabbit76" on stackoverflow.
 
-        #If the score exceeds 50
+        # If the score exceeds 50
         if score > 10:
             for rock in rock_group:
                 rock.move(0, -1/80*rock.rock_fall_y)
@@ -320,10 +331,10 @@ def main():
                     rock.rock_fall_y = 0
 
 
-        #clear the display
+        # clear the display
         screen.blit(background,(0,0))
 
-        #Update elements
+        # Update elements
         player_group.update()
         block_group.update()
         block_group.draw(screen)
@@ -332,13 +343,16 @@ def main():
         rock_group.update(screen)
 
 
-        show_score()
-        show_button()
+        show_score(score)
+        high_score = set_high_score(high_score, score)
+        show_sound_control()
 
         #If dog falls out of screen, display gameover message.
         if gameover:
             gameover_text()
             show_score(BLACK,120,300)
+            pygame.mixer.music.pause()
+
 
         #Update
         pygame.display.flip()
